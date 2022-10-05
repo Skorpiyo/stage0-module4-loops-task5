1d6a1d6eee7ca46df2880078d5eddb4290fb3338
Skorpiyo <skorpio277@gmail,com>
2022-10-05 18:59:46 +0300

    so hard

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int y = 1; y <= sideLength; y++) {
+            for (int x = 1; x <= sideLength; x++) {
+                int sum=(sideLength+1)/2;
+                if (x == 0 || x == sum || y==0 || y==sum) {
+                    System.out.print("8");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
+
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < height; x++) {
+                int sum=height/2;
+                if (y<sum && x>=y && x<height-y) {
+                    System.out.print("8");
+                }else if(y>=sum && x<=y && x>=height-y-1) {
+                    System.out.print("8");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
